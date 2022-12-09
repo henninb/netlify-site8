3eb16c56a8ece6126c3d5ede17f02e0f27e0c618
Brian Henning <brian.henning@humansecurity.com>
2022-12-09 14:37:48 -0600

    wip

--- public/tool1/script.js
+++ public/tool1/script.js
@@ -1,14 +1,33 @@
 window.dataLayer = window.dataLayer || [];
 
-console.log('here is my test');
+function getCookie(name) {
+    var re = new RegExp(name + "=([^;]+)");
+    var value = re.exec(document.cookie);
+    if (value != null) {
+        const obj = JSON.parse(unescape(value[1]));
 
-function getJsonFromUrl(url) {
-  if(!url) url = location.search;
-  var query = url.substr(1);
-  var result = {};
-  query.split("&").forEach(function(part) {
-    var item = part.split("=");
-    result[item[0]] = decodeURIComponent(item[1]);
-  });
-  return result;
+        return (obj.id != null) ? (obj.id) : null;
+    }
+    return (null);
 }
+
+function getParameter(paramName) {
+  var searchString = window.location.search.substring(1),
+      i, val, params = searchString.split("&");
+
+  for (i=0;i<params.length;i++) {
+    val = params[i].split("=");
+    if (val[0] == paramName) {
+      return val[1];
+    }
+  }
+  return null;
+}
+
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+
+console.log(uuidv4());
