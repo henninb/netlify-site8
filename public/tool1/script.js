2cdfe2d2e61546c69fd25f9f229b6c134121a82d
Brian Henning <henninb@msn.com>
2022-12-09 05:28:30 -0600

    wip

--- public/tool1/script.js
+++ public/tool1/script.js
@@ -1,3 +1,14 @@
 window.dataLayer = window.dataLayer || [];
 
 console.log('here is my test');
+
+function getJsonFromUrl(url) {
+  if(!url) url = location.search;
+  var query = url.substr(1);
+  var result = {};
+  query.split("&").forEach(function(part) {
+    var item = part.split("=");
+    result[item[0]] = decodeURIComponent(item[1]);
+  });
+  return result;
+}
