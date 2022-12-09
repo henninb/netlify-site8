34bdee151b5800185108e6319fb109fcc757ea23
Brian Henning <brian.henning@humansecurity.com>
2022-12-09 14:59:57 -0600

    wip

--- public/tool1/late.js
+++ public/tool1/late.js
@@ -1,8 +1,10 @@
+window.document.getElementById("guid").value = uuidv4();
+
 function setValue(id,newvalue) {
   var s = document.getElementById(id);
   s.value = newvalue;
 }
 
-window.onload=function() {
-  setValue("guid", uuidv4());
-}
+// window.onload=function() {
+//   setValue("guid", uuidv4());
+// }
