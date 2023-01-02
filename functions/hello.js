d4c4acff8a5ab4cd70ff2782fd34795e316b662d
Brian Henning <henninb@msn.com>
2023-01-02 13:05:28 -0600

    wip

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,6 @@
-exports.handler = (event, context, callback) => {
-  const message = 'Hello, World!';
-  callback(null, {
+exports.handler = (event, context) => {
+    return {
     statusCode: 200,
-    body: "hello",
-  });
+    body: "testing"
+  }
 };
