865b8cf18fa77584cb708b0db25b5a38796f4b63
Brian Henning <henninb@msn.com>
2023-01-02 11:44:48 -0600

    wip

--- run.sh
+++ run.sh
@@ -8,4 +8,9 @@ docker rmi nginx-server
 docker-compose build
 docker-compose up -d
 
+echo netlify dev
+echo netlify function:create
+echo netlify functions:invoke
+# echo npx netlify-lambda build src
+
 exit 0
